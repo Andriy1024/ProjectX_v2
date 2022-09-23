ff910fb29c057c2f7d1a9486549b494196412cab
Andrii Zubyk <AZubyk@slb.com>
2022-09-24 01:09:43 +0200

    implement tab transitions

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,94 @@
+import { trigger, transition, animate, style, query, group } from '@angular/animations';
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('routeAmin', [
+      transition(':increment', [
+        style({
+          position: 'relative',
+          overflow: 'hidden'
+        }),
+
+        query(':enter, :leave', [
+          style({
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%'
+          })
+        ], { optional: true }),
+
+        group([
+          query(':leave', [ 
+            animate('200ms ease-in', style({ 
+              opacity: 0,
+              transform: 'translateX(-100px)' 
+            })) 
+          ], { optional: true }),
+  
+          query(':enter', [
+            style({ 
+              transform: 'translateX(100px)', 
+              opacity: 0 }),
+            animate('250ms 120ms ease-out', style({ 
+              opacity: 1,
+              transform: 'translateX(0px)' 
+            }))
+          ], { optional: true })
+        ])
+      ]),
+
+      transition(':decrement', [
+        style({
+          position: 'relative',
+          overflow: 'hidden'
+        }),
+
+        query(':enter, :leave', [
+          style({
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%'
+          })
+        ], { optional: true }),
+
+        group([
+          query(':leave', [ 
+            animate('200ms ease-in', style({ 
+              opacity: 0,
+              transform: 'translateX(100px)' 
+            })) 
+          ], { optional: true }),
+  
+          query(':enter', [
+            style({ 
+              transform: 'translateX(-100px)', 
+              opacity: 0 }),
+            animate('250ms 120ms ease-out', style({ 
+              opacity: 1,
+              transform: 'translateX(0px)' 
+            }))
+          ], { optional: true })
+        ])
+      ])
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'ProjectX';
+  
+  prepareRoute(outlet: RouterOutlet) {
+    if (outlet.isActivated) {
+      return outlet.activatedRouteData['tab'];
+    }
+
+    return undefined;
+  }
 }
