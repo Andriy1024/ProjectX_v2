1b5aa278623162d29aa27b45288fd41a1318fc63
Andrii <andriyzybyk@gmail.com>
2022-11-07 00:02:57 +0100

    extend IQueryableExtensions

--- backend/Common/ProjectX.Persistence/Extensions/IQueryableExtensions.cs
+++ backend/Common/ProjectX.Persistence/Extensions/IQueryableExtensions.cs
@@ -19,6 +19,47 @@ public static class IQueryableExtensions
         return reverse ? source.OrderByDescending(propertyName) : source.OrderBy(propertyName);
     }
 
+    public static IQueryable<TEntity> Where<TEntity, TValue>(this IQueryable<TEntity> source, string propertyName, TValue value) 
+    {
+        // t
+        ParameterExpression parameter = Expression.Parameter(typeof(TEntity), "t");
+        // t.Name
+        MemberExpression property = Expression.Property(parameter, propertyName);
+        // value
+        ConstantExpression constant = Expression.Constant(value);
+        // t.Name == "value"
+        BinaryExpression filter = Expression.Equal(property, constant);
+        // t => t.Name == "value"
+        var predicate = Expression.Lambda<Func<TEntity, bool>>(filter, parameter);
+
+        return source.Where(predicate);
+    }
+
+    /// <summary>
+    /// The value is parsed to the type of property
+    ///  where(t => t.Id == int.parse("1"));
+    ///  where(t => t.property == [type].parse(value))
+    /// </summary>
+    public static IQueryable<TEntity> DynamicWhere<TEntity>(this IQueryable<TEntity> source, string propertyName, string value)
+    {
+        // t
+        ParameterExpression parameter = Expression.Parameter(typeof(TEntity), "t");
+        // t.Name
+        MemberExpression property = Expression.Property(parameter, propertyName);
+        // [type].Parse(value)
+        PropertyInfo properyInfo = property.Member as PropertyInfo;
+        MethodInfo parseMethod = properyInfo.PropertyType.GetMethod("Parse", new[] { typeof(string) });
+        ConstantExpression constant = Expression.Constant(value);
+        MethodCallExpression parseCall = Expression.Call(parseMethod, constant);
+
+        // t.property == [type].parse(value)
+        BinaryExpression filter = Expression.Equal(property, parseCall);
+        // t => t.Name == "value"
+        var predicate = Expression.Lambda<Func<TEntity, bool>>(filter, parameter);
+
+        return source.Where(predicate);
+    }
+
     private static IOrderedQueryable<T> OrderBy<T>(this IQueryable<T> source, string propertyName)
         => source.OrderBy(CreateExpression<T>(propertyName));
 
@@ -27,8 +68,8 @@ public static class IQueryableExtensions
 
     private static Expression<Func<T, object>> CreateExpression<T>(string propertyName)
     {
-        var parameter = Expression.Parameter(typeof(T));
-        var property = Expression.Property(parameter, propertyName);
+        ParameterExpression parameter = Expression.Parameter(typeof(T));
+        MemberExpression property = Expression.Property(parameter, propertyName);
         var propAsObject = Expression.Convert(property, typeof(object));
 
         return Expression.Lambda<Func<T, object>>(propAsObject, parameter);
