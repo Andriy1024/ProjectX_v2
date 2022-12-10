07bb35a98684f0e15cfa727c3cb49f8f891bffe5
Andrii <andriyzybyk@gmail.com>
2022-12-10 19:15:57 +0100

    remove note add component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NoteAddComponent } from './note-add/note-add.component';
-import { BookmarksComponent } from './bookmarks/bookmarks.component';
+import { DynamicFormComponent } from './components/dynamic-form/dynamic-form.component';
+
 import { NotesComponent } from './notes/notes.component';
-import { TodosComponent } from './todos/todos.component';
 import { NoteEditComponent } from './note-edit/note-edit.component';
+
+import { TodosComponent } from './todos/todos.component';
 import { TodoAddComponent } from './todo-add/todo-add.component';
 import { TodoEditComponent } from './todo-edit/todo-edit.component';
+
+import { BookmarksComponent } from './bookmarks/bookmarks.component';
 import { BookmarkAddComponent } from './bookmark-add/bookmark-add.component';
 import { BookmarksManageComponent } from './bookmarks-manage/bookmarks-manage.component';
 import { BookmarkEditComponent } from './bookmark-edit/bookmark-edit.component';
-import { DynamicFormComponent } from './components/dynamic-form/dynamic-form.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/bookmarks', pathMatch: 'full' },
@@ -23,9 +25,7 @@ const routes: Routes = [
   { path: 'todos/add', component: TodoAddComponent },
   { path: 'todos/:id', component: TodoEditComponent },
   { path: 'notes', component: NotesComponent, data: { tab: 3 } },
-  { path: 'notes/add', component: NoteAddComponent },
   { path: 'notes/:id', component: NoteEditComponent },
-
   { path: 'form', component: DynamicFormComponent }
 ];
 
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,47 +1,50 @@
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { TabsComponent } from './tabs/tabs.component';
-import { BookmarksComponent } from './bookmarks/bookmarks.component';
-import { TodosComponent } from './todos/todos.component';
+import { NotificationComponent } from './notification/notification.component';
+import { DynamicFormComponent } from './components/dynamic-form/dynamic-form.component';
+
 import { NotesComponent } from './notes/notes.component';
-import { BookmarkTileComponent } from './bookmark-tile/bookmark-tile.component';
-import { NoteAddComponent } from './note-add/note-add.component';
 import { NoteCardComponent } from './note-card/note-card.component';
 import { NoteEditComponent } from './note-edit/note-edit.component';
+
+import { TodosComponent } from './todos/todos.component';
 import { TodoItemComponent } from './todo-item/todo-item.component';
 import { TodoAddComponent } from './todo-add/todo-add.component';
 import { TodoEditComponent } from './todo-edit/todo-edit.component';
+
+import { BookmarksComponent } from './bookmarks/bookmarks.component';
 import { BookmarkAddComponent } from './bookmark-add/bookmark-add.component';
 import { BookmarksManageComponent } from './bookmarks-manage/bookmarks-manage.component';
 import { BookmarkEditComponent } from './bookmark-edit/bookmark-edit.component';
-import { NotificationComponent } from './notification/notification.component';
-import { DynamicFormComponent } from './components/dynamic-form/dynamic-form.component';
+import { BookmarkTileComponent } from './bookmark-tile/bookmark-tile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TabsComponent,
+    NotificationComponent,
+    DynamicFormComponent,
+
     BookmarksComponent,
-    TodosComponent,
-    NotesComponent,
-    BookmarkTileComponent,
-    NoteAddComponent,
-    NoteCardComponent,
-    NoteEditComponent,
-    TodoItemComponent,
-    TodoAddComponent,
-    TodoEditComponent,
     BookmarkAddComponent,
     BookmarksManageComponent,
     BookmarkEditComponent,
-    NotificationComponent,
-    DynamicFormComponent
+    BookmarkTileComponent,
+
+    TodosComponent,
+    TodoItemComponent,
+    TodoAddComponent,
+    TodoEditComponent,
+
+    NotesComponent,
+    NoteCardComponent,
+    NoteEditComponent,
   ],
   imports: [
     BrowserModule,
